5313dd64f1717d6d819a35f4d9d1b26cdf5dca03
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-12 10:59:13 -0500

    ejercicio2

--- taller2_condicionales.py
+++ taller2_condicionales.py
@@ -27,4 +27,38 @@ else:
     print(f"El descuento aplicado es: ${descuento:,}")
     print(f"El valor unitario es: ${precio_camisa1:,}")
     print(f"La cantidad comprade es: {cantidad_camisa1}")
+
+
+# 2. En un supermercado se hace una promoción, mediante la cual el
+# cliente obtiene un descuento dependiendo de un número que se
+# escoge al azar. Si el número escogido es menor que 74 el descuento
+# es del 15% sobre el total de la compra, si es mayor o igual a 74 el
+# descuento es del 20%. Obtener cuanto dinero se le descuenta.
+
+precio_compra2 = float(input("Digite el total de la compra: $"))
+nuemo_azar2 = int(input("Digite un nuemro al azar mayor o igual a 0: "))
+# total_camisas_pagar = precio_camisa1 * cantidad_camisa1
+descuento = 0.0
+if(precio_compra2 < 0 or nuemo_azar2 < 0):
+    print("Por favor verificar datos digitados, los datos no pueden ser menor a 0")
+    print(f" precio de la compra ${precio_compra2:,}")
+    print(f" nuemro al azar digitado:{nuemo_azar2}")
+else:
+    if(nuemo_azar2 < 74):
+        descuento = precio_compra2 * 0.15
+    else:
+        descuento = precio_compra2 * 0.2
+    total_a_pagar_descuento = precio_compra2 - descuento
+    print(f"El total a pagar es: ${total_a_pagar_descuento:,}")
+    print(f"El descuento aplicado es: ${descuento:,}")
+    print(f"El valor dela compra era de: ${precio_compra2:,}")
+    print(f"El nuemro al azar digitado es: {nuemo_azar2}")
+
   
+    
+  
+    
+  
+    
+  
+    
